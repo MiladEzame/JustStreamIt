01b920e18f8871fff9847b9969651ff4ed98ed38
MiladEzame <milad.ezame@gmail.com>
2021-07-20 00:05:14 +0200

    modified project and added SASS, modified script, retrieves correct data but needs to be aligned like shown in the wireframe using css

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var modalBtn = document.querySelector(".modal-btn");
+var modalBg = document.querySelector(".modal-bg")
+var modalclose = document.querySelector(".modal-close")
 
-// Get the button that opens the modal
-var btn = document.getElementById("button1");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+modalBtn.addEventListener("click", function(){
+    modalBg.classList.add('bg-active')
+})
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+modalclose.addEventListener("click", function(){
+    modalBg.classList.remove('bg-active')
+})
+
+function show_output(output, movie){
+    output += "<div class='title'> " +
+    "<h1>"  + movie.title + '</h1></div>' + 
+    "<div class='release-info'> " +
+    "<table><tr><th>Genre: "  + movie.genres + "</th>" +
+    "<th>Published: "  + movie.date_published + "</th>" +
+    "<th>Duration: "  + movie.duration + "</th></tr></table></div>" +
+    "<div class='ratings'> " +
+    "<table><tr><th>Rated: "  + movie.rated + "</th>" +
+    "<th>IMDB score: "  + movie.imdb_score + "</th>" +
+    "<th>Box Office Rating: "  + movie.avg_vote + "</th></tr></table></div>" +
+    "<p>" + movie.description + "</p>" +
+    "Casting: " + movie.actors;
+    return output
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-    modal.style.display = "none";
-    }
-}
-
-document.getElementById('button1').addEventListener('click', loadMovie);
+modalBtn.addEventListener("click", loadMovie);
 
 function loadMovie(){
 
@@ -35,21 +38,13 @@ function loadMovie(){
     if(this.status == 200){
         var movie = JSON.parse(this.responseText);
         
+        var output2 = '';
+        output2 += '<img src = "' + movie.image_url + '"</img src>'
+        document.getElementById("img-movie").innerHTML = output2
+
         var output = '';
-        output += '<ul>' + 
-        '<img src = "' + movie.image_url + '"</img src>' + 
-        '<li>Genre(s): ' + movie.genres + '</li>' + 
-        '<li>Published : ' + movie.date_published + '</li>' + 
-        '<li>Title : ' + movie.title + '</li>' + 
-        '<li>Rated : ' + movie.rated + '</li>' + 
-        '<li>IMDB Score : ' + movie.imdb_score + '</li>' +
-        '<li>Director(s) : ' + movie.directors + '</li>' + 
-        '<li>Actors : ' + movie.actors + '</li>' + 
-        '<li>Duration : ' + movie.duration + '</li>' + 
-        '<li>Box Office Result : ' + movie.avg_vote + '</li>' + 
-        '<li>Description : ' + movie.description + '</li>'        
-        '</ul>';
-        document.getElementById('movie').innerHTML = output;
+        
+        document.getElementById('movie').innerHTML = show_output(output, movie);
         }
     }
     
